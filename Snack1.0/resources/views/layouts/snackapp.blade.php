cb79d56f5c97a466ba431dcd1b3daa6c1ba4caa9
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2023-08-14 08:21:28 -0700

    スマホ版対応 first

--- Snack1.0/resources/views/layouts/snackapp.blade.php
+++ Snack1.0/resources/views/layouts/snackapp.blade.php
@@ -6,6 +6,7 @@
         <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
         <!--csrf token-->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>@yield('title')</title>
         <style>
--- Snack1.0/resources/views/layouts/snackapp_mypage.blade.php
+++ Snack1.0/resources/views/layouts/snackapp_mypage.blade.php
@@ -4,6 +4,8 @@
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <!--イイネ機能に向けたcsrf_tokenの設定--> 
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <title>@yield('title')</title>
 
     
